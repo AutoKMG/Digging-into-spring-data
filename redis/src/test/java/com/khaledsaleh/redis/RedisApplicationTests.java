16f76f49bc9a6d8aeff9d2083cccf00a81e2a0ef
Khaled Saleh <th.be03@gmail.com>
2023-10-11 19:48:17 +0200

    Adding the first files for template-based service with redis

---
+++ redis/src/test/java/com/khaledsaleh/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaledsaleh.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
